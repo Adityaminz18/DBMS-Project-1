221a26642ac681ec131862a2a48a512e662f70d1
Aditya Minz <98016435+Adityaminz18@users.noreply.github.com>
2024-03-18 12:23:29 +0530

    Added Documentation PDF

--- Resources/Backend/Projects/Project2/blog-app/main.py
+++ Resources/Backend/Projects/Project2/blog-app/main.py
@@ -9,3 +9,13 @@ app = FastAPI()
 @app.post("/blog")
 def create(request:schemas.Blog):
     return request
+
+@app.post("/update")
+def update():
+    # update the existing blog
+    pass
+
+@app.get("/blog")
+def get():
+    #fetch all the blog from the database
+    pass
--- Resources/Backend/Projects/Project2/blog-app/schemas.py
+++ Resources/Backend/Projects/Project2/blog-app/schemas.py
@@ -3,3 +3,5 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body:str
+
+
